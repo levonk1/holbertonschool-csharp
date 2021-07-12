d234683aa0b969e732c89e8732860a6660708273
levonk1 <87328513+levonk1@users.noreply.github.com>
2021-07-13 00:00:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace writeline
+{
+    class writeline
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
