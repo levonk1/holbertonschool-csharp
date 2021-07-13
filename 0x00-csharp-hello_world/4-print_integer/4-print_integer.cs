41f3cdc0749595c0aa23f0210906c479a781248e
levonk1 <87328513+levonk1@users.noreply.github.com>
2021-07-13 18:21:24 +0300

    Update 4-print_integer.cs

--- 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        int number = 972;
-	Console.WriteLine("{0} Mission Street", number);
-    }
+	static void Main(string[] args)
+	{
+		int number = 972;
+		Console.WriteLine("{0} Mission Street", number);
+	}
 }
