8bfaa5852595a6ddb6932dfc2a4121a6b0fc1675
levonk1 <87328513+levonk1@users.noreply.github.com>
2021-07-29 21:05:14 +0300

    Add files via upload

---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyMath
+{
+    ///<summary>Performs operations on 2 dimensional matrixies.</summary>
+    public class Matrix
+    {
+        ///<summary>Divides elements in a matrix by a number.</summary>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (matrix == null)
+            {
+                return (null);
+            }
+            if (num == 0)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return (null);
+            }
+            int mHeight = matrix.GetLength(0);
+            int mWidth = matrix.GetLength(1);
+            int[,] newMatrix = new int[mHeight, mWidth];
+            for (int i = 0; i < mHeight; i++)
+            {
+                for (int j = 0; j < mWidth; j++)
+                {
+                    newMatrix[i, j] = matrix[i,j] / num;
+                }
+            }
+            return (newMatrix);
+        }
+    }
+}
